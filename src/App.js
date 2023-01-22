7d4402dd6045d6589781744de3ea1e81d56979b4
LuchoD99 <luciano.diazocampo@gmail.com>
2023-01-21 21:36:36 -0300

    update edit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
-import Card from './components/Card/Card';
 import Cards from './components/Cards/Cards';
 import Search from './components/Search/Search';
-import data, { Cairns } from './data';
+// import data, { Cairns } from './data';
 
 function App() {
+    // const [ciudad, setCiudad] = useState([]);
+    // function onSearch(ciudad) {
+    //     fetch(
+    //         `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=61976988447f1f24833b2e7d8d8a2499&units=metric`
+    //     )
+    //         .then((e) => e.json())
+    //         .then((resp) => {
+    //             if (resp.cod === '404') {
+    //                 alert('Ciudad no encontrada');
+    //             } else {
+    //                 const ciudad = {
+    //                     // se crea un obj
+    //                     min: Math.round(resp.main.temp_min),
+    //                     max: Math.round(resp.main.temp_max),
+    //                     img: resp.weather[0].icon,
+    //                     id: resp.id,
+    //                     wind: resp.wind.speed,
+    //                     temp: resp.main.temp,
+    //                     name: resp.name,
+    //                     weather: resp.weather[0].main,
+    //                     clouds: resp.clouds.all,
+    //                     latitud: resp.coord.lat,
+    //                     longitud: resp.coord.lon,
+    //                 };
+    //                 setCiudad((Allcities) => [...Allcities, ciudad]);
+    //             }
+    //         });
+    // }
+
+    // function onClose(id) {}
     return (
         <div className="App">
-            <div>
-                <Search onSearch={(ciudad) => alert(ciudad)} />
-            </div>
-            <div>
-                <Card
-                    max={Cairns.main.temp_max}
-                    min={Cairns.main.temp_min}
-                    name={Cairns.name}
-                    img={Cairns.weather[0].icon}
-                    onClose={() => alert(Cairns.name)}
-                />
-            </div>
-            <hr />
-            <div>
-                <Cards cities={data} />
-            </div>
-            <hr />
+            <Search />
+            <Cards />
         </div>
     );
 }
