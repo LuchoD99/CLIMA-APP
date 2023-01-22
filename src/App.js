e3d0baa9f03382328f9526e83c40a8ea822bdbcb
LuchoD99 <luciano.diazocampo@gmail.com>
2023-01-21 21:02:34 -0300

    update search

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 // import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
+import Cards from './components/Cards/Cards';
+import Search from './components/Search/Search';
+import data, { Cairns } from './data';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <div>
+                <Search onSearch={(ciudad) => alert(ciudad)} />
+            </div>
+            <div>
+                <Card
+                    max={Cairns.main.temp_max}
+                    min={Cairns.main.temp_min}
+                    name={Cairns.name}
+                    img={Cairns.weather[0].icon}
+                    onClose={() => alert(Cairns.name)}
+                />
+            </div>
+            <hr />
+            <div>
+                <Cards cities={data} />
+            </div>
+            <hr />
         </div>
     );
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import './Search.css';
 export default function Search(prop) {
     return (
-        <div>
-            <input type="text" placeholder="Ciudad.." />
-            <button onClick={() => prop.onSearch('Buscando...')}>
+        <div className="center">
+            <input className="texto" type="text" placeholder="Ciudad.." />
+            <button
+                className="boton"
+                onClick={() => prop.onSearch('Buscando...')}
+            >
                 Agregar
             </button>
         </div>
