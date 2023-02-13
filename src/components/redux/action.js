1100f86ae767e6995fe4474b9cf992d9a08569bd
LuchoD99 <luciano.diazocampo@gmail.com>
2023-02-13 16:28:11 -0300

    cambios

--- src/components/redux/action.js
+++ src/components/redux/action.js
@@ -1,37 +1,62 @@
+import axios from 'axios';
 export const CIUDAD_CARGADA = 'CIUDAD_CARGADA';
 export const ELIMINAR_CIUDAD = 'ELIMINAR_CIUDAD';
 export const CIUDAD_DETALLES = 'CIUDAD_DETALLES';
 export const VACIAR_DETALLES = 'VACIAR_DETALLES';
-
 export function Ciudades(ciudad) {
-    return function (dispatch) {
-        fetch(
-            `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=61976988447f1f24833b2e7d8d8a2499&units=metric`
-        )
-            .then((e) => e.json())
-            .then((data) => {
-                const ciudad = {
-                    // se crea un obj
-                    min: Math.round(data.main.temp_min),
-                    max: Math.round(data.main.temp_max),
-                    img: data.weather[0].icon,
-                    id: data.id,
-                    wind: data.wind.speed,
-                    temp: data.main.temp,
-                    name: data.name,
-                    weather: data.weather[0].main,
-                    clouds: data.clouds.all,
-                    latitud: data.coord.lat,
-                    longitud: data.coord.lon,
-                };
-                dispatch({
-                    type: CIUDAD_CARGADA,
-                    payload: ciudad,
-                });
-            })
-            .catch((err) => {
-                alert('Ciudad no encontrada');
+    return async function (dispatch) {
+        // fetch(
+        //     `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=61976988447f1f24833b2e7d8d8a2499&units=metric`
+        // )
+        //     .then((e) => e.json())
+        //     .then((data) => {
+        //         let ciudad = {
+        //             // se crea un obj
+        //             min: Math.round(data.main.temp_min),
+        //             max: Math.round(data.main.temp_max),
+        //             img: data.weather[0].icon,
+        //             id: data.id,
+        //             wind: data.wind.speed,
+        //             temp: data.main.temp,
+        //             name: data.name,
+        //             weather: data.weather[0].main,
+        //             clouds: data.clouds.all,
+        //             latitud: data.coord.lat,
+        //             longitud: data.coord.lon,
+        //         };
+        //         dispatch({
+        //             type: CIUDAD_CARGADA,
+        //             payload: ciudad,
+        //         });
+        //     })
+        //     .catch((err) => {
+        //         alert('Ciudad no encontrada');
+        //     });
+        try {
+            let api = await axios.get(
+                `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=61976988447f1f24833b2e7d8d8a2499&units=metric`
+            );
+            let datos = api.data;
+            let result = {
+                min: Math.round(datos.main.temp_min),
+                max: Math.round(datos.main.temp_max),
+                img: datos.weather[0].icon,
+                id: datos.id,
+                wind: datos.wind.speed,
+                temp: datos.main.temp,
+                name: datos.name,
+                weather: datos.weather[0].main,
+                clouds: datos.clouds.all,
+                latitud: datos.coord.lat,
+                longitud: datos.coord.lon,
+            };
+            dispatch({
+                type: CIUDAD_CARGADA,
+                payload: result,
             });
+        } catch (error) {
+            alert('Ciudad no encontrada');
+        }
     };
 }
 
